13e2b6af451b61a098ec4f256e99f29420fa384d
Armando1509 <armandogomez1509@gmail.com>
2023-05-03 22:48:23 -0600

    deploy

--- Api/index.js
+++ Api/index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("%s aqui entras at 3001"); // eslint-disable-line no-console
   });
 });
